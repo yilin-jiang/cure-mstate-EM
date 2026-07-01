992ea777e2e1b97368543278cf9272e02d7a2870
yilin-jiang <140752694+yilin-jiang@users.noreply.github.com>
2026-07-01 18:12:22 +0200

    Delete functions directory

--- functions/add_likelihood.R
+++
@@ -1,5 +0,0 @@
-add_likelihood=function(data){
-  #data should include column hazard, status, cumHaz
-  data$lik=(data$hazard^data$status)*exp(-data$cumHaz)
-  return(data)
-}
--- functions/update_pi.R
+++
@@ -1,7 +0,0 @@
-update_pi=function(fit_alpha,cov,data_longlong){
-
-  data_longlong$pi = predict(fit_alpha, newdata=data_longlong[,cov],
-                          type="response")
-  return(data_longlong)
-
-}
