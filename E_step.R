6a7be34123708b26ef86d2681c90a62cc24dd1d9
yilin-jiang <140752694+yilin-jiang@users.noreply.github.com>
2026-07-01 19:25:46 +0200

    Delete E_step.R

--- E_step.R
+++
@@ -1,8 +0,0 @@
-gendataboot=function(data,seed){
-  set.seed(seed)
-  N=nrow(data)
-  id_sample=sample(1:N,N,replace=TRUE)
-  data_boot=data[id_sample,]
-  rownames(data_boot)=1:N
-  return(data_boot)
-}
